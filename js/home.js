fb5f2a19717cca8c0c37d6c940af52236cdcd9e4
Cala Ruggeri <calaruggeri@MacBook-Pro-de-Cala.local>
2020-06-06 19:46:23 -0300

    cambios

--- js/home.js
+++ js/home.js
@@ -1,1 +1,32 @@
 
+window.onload = function () {
+
+fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/artists")
+
+.then(function(response){
+    return response.json()
+}).then(function(data){
+    console.log(data);
+    
+        let artistas = data.data.artistas
+        let contenedorArtistas = document.querySelector(".artista");
+        
+        for (const artista of artistas) {
+            contenedorArtistas.innerHTML +=`
+             
+        <div>
+        <a href="detallehome.html"> <img class="photo" src="${artista.picture_big}" alt=""></a>
+             <div>
+                <h2>${artista.name}</h2> </a>
+             </div>
+        </div>
+             
+            `
+        }
+      
+})
+.catch(function(error){
+    console.error(error)
+})
+
+}
