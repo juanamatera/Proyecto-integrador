8f547daa89a805fd7e4a9cd32967a5a1905ab9c0
Daira Sapir <dairasapir@MacBook-Air-de-Daira.local>
2020-06-15 20:17:37 -0300

    ya termine el detalle de albummmmmmm

--- js/album.js
+++ js/album.js
@@ -2,7 +2,7 @@ window.onload = function () {
 
 var queryString = location.search;
 var queryStringObj = new URLSearchParams(queryString);
-queryStringObj.get = ("albumID")
+var id = queryStringObj.get ("IdAlbum")
 
 fetch ("https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/" + id)
 
@@ -29,6 +29,44 @@ fetch ("https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/" + id)
     </article>`
 
     contenedorAlbum.innerHTML = detalleAlbum
+
+
+    fetch ("https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/" + id + "/tracks")
+
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(resultados){
+        console.log(resultados);
+
+        let cancionesArray = resultados.data
+        let contenedorCanciones = document.querySelector(".canciones");
+        let DivPhotoContainer = '';
+
+        console.log(cancionesArray);
+
+        for (let i = 0; i < 5; i++) {
+            let cancion = cancionesArray[i];
+
+            console.log(cancion.duration);
+            
+            durationMin = Math.floor(cancion.duration/60);
+                durationSec = cancion.duration - durationMin*60;
+                console.log(durationSec);
+                if (durationSec < 10) {
+                    durationSec = '0' + durationSec;
+                }
+            DivPhotoContainer += `
+            <div class="a">
+            <p class="heart"><i class="fa fa-heart"></i></p>
+            <a href="detalletracks.html?IdCancion=${cancion.id}"><p class="titulo">${cancion.title}</p></a>
+            <p>${durationMin}: ${durationSec} </p> 
+             </div>
+            `;
+        }        
+
+        contenedorCanciones.innerHTML += DivPhotoContainer
+    })
 })
 
 .catch(function(error){
