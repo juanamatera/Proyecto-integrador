290f992cfb9f1e146dca66d35d37a6f9a52b08d3
Daira Sapir <dairasapir@MacBook-Air-de-Daira.local>
2020-06-18 21:02:10 -0300

    faltan algunos detalles de playlist (js)

--- js/playlist.js
+++ js/playlist.js
@@ -1,1 +1,46 @@
 
+window.onload = function () {
+
+
+if (window.localStorage.getItem ("listId") === null) {
+
+    
+    
+} else {
+
+    var playlist = JSON.parse (window.localStorage.getItem ("listId"))
+
+    let track = document.querySelector(".canciones")
+    
+    
+    for (const id of playlist) {
+
+        var cancion = JSON.parse (window.localStorage.getItem (`${id}`))
+        console.log(cancion)
+
+        durationMin = Math.floor(cancion.duration/60);
+                durationSec = cancion.duration - durationMin*60;
+                console.log(durationSec);
+                if (durationSec < 10) {
+                    durationSec = '0' + durationSec;
+                }
+
+        track.innerHTML += `
+        <div class="a">
+        <p class="heart"><i class="fa fa-heart"></i></p>
+        <a href="detalletracks.html?IdTrack=${cancion.id}">
+        <p class="titulo">${cancion.title}</p>
+        </a>
+        <p class="artista">${cancion.artist.name}</p>
+        <p class="tiempo">${durationMin}: ${durationSec}</p>
+        </div> `
+        
+    }
+
+
+    
+}
+
+
+
+}
